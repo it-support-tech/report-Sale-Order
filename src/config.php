02ca7c140533daacb61cd1aeccf73cba98db1639
it-support-tech <it-support@ntp-lao.com>
2026-08-21 09:20:36 +0700

    change margin

--- src/config.php
+++ src/config.php
@@ -1,21 +1,21 @@
 <?php
 
-return [
-    'db' => [
-        'host' => 'localhost',
-        'port' => '5432',
-        'name' => 'SaleOrder',
-        'user' => 'ntp2026',
-        'pass' => 'admin@123#',
-    ],
-];
-
 // return [
 //     'db' => [
-//         'host' =>  'db',
-//         'port' =>  '5432',
-//         'name' =>  'reportdn',
-//         'user' => 'reportdn',
-//         'pass' => 'reportdn',
+//         'host' => 'localhost',
+//         'port' => '5432',
+//         'name' => 'SaleOrder',
+//         'user' => 'ntp2026',
+//         'pass' => 'admin@123#',
 //     ],
 // ];
+
+return [
+    'db' => [
+        'host' =>  'db',
+        'port' =>  '5432',
+        'name' =>  'reportdn',
+        'user' => 'reportdn',
+        'pass' => 'reportdn',
+    ],
+];
